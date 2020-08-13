e237674b00fb6a35f502e60cdc5935140bb45776
jle290 <jong.lee@nike.com>
2020-08-13 11:39:00 -0700

    mapping correct attributes

--- routes/provider_state.js
+++ routes/provider_state.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 router.route('/')
   .post(function (req, res) {
-    const consumer = req.query['consumer'];
-    const providerState = req.query['state'];
+    const consumer = req.body.consumer;
+    const providerState = req.body.state;
     // imagine we're setting the server into a certain state
+    console.log(`changed to provider state "${providerState}" for consumer "${consumer}"`);
     res.send(`changed to provider state "${providerState}" for consumer "${consumer}"`);
     res.status(200);
   });
